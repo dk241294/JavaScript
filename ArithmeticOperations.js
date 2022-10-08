502c890ad6ec2b59f7736a9661885a868fff1c48
dk241294 <deepak24121994@gmail.com>
2022-10-08 18:27:29 +0530

    Arithmetic Operations of two number

---
+++ ArithmeticOperations.js
@@ -0,0 +1,9 @@
+let x=5
+let y=7
+
+
+console.log("Addition of two number is " +(x+y));
+console.log("Subtraction of two number is "+(x-y));
+console.log(("Multiplication of two number is "+(x*y)));
+console.log ("Division of Two number "+ (x/y));
+console.log("exponent of two number "+(x**y));
