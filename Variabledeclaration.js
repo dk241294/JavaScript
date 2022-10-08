b095221c9e4f4287835b774927c5b1b1cf2f61ef
dk241294 <deepak24121994@gmail.com>
2022-10-08 17:52:14 +0530

    Variable declaration in js

---
+++ Variabledeclaration.js
@@ -0,0 +1,30 @@
+//follow the camelCase naming convention
+
+//declaration and initialization
+let num1 = 5;
+let num2 = 4;
+let sum=0;
+console.log(num1+num2);
+
+//Incase of let be canot redeclare 
+
+var num3=2;
+console.log(num3)
+var num3=4;
+console.log(num3)
+
+//Incase of var be can redeclare the same variable 
+
+
+const pi=3.14
+//pi=9.445; 
+//constant cannot be changed.
+console.log(pi)
+var r =4
+var areaOfCircle=pi*r*r
+console.log(areaOfCircle)
+var perimeterOfCircle=2*pi*r
+console.log(perimeterOfCircle)
+
+// const variable are constant that is not changed througout
+
