92af4d90a8466c4a2ca7f883df58d9a4b92d8aa5
dk241294 <deepak24121994@gmail.com>
2022-10-08 23:43:53 +0530

    Increment and Decrement

--- ArithmeticOperations.js
+++ ArithmeticOperations.js
@@ -7,3 +7,13 @@ console.log("Subtraction of two number is "+(x-y));
 console.log(("Multiplication of two number is "+(x*y)));
 console.log ("Division of Two number "+ (x/y));
 console.log("exponent of two number "+(x**y));
+console.log('Division of two number '+ x % y);   
+// increment
+console.log('++x = ', ++x); // x is now 6
+console.log('x++ = ', x++); // prints 6 and then increased to 7
+console.log('x = ', x);     // 7
+// decrement
+console.log('--x = ', --x); // x is now 6
+console.log('x-- = ', x--); // prints 6 and then decreased to 5
+console.log('x = ', x);     // 5
+
