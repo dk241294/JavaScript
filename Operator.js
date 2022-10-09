582724311998506537ff4192491ecca1e2e88533
dk241294 <deepak24121994@gmail.com>
2022-10-09 18:56:03 +0530

    Logical and comparison operator

---
+++ Operator.js
@@ -0,0 +1,19 @@
+var x=2 
+var y=2
+//Comparison Operators 
+console.log ('x==y ' + (x==y))
+console.log('x>y ' +(x>y))
+console.log("x<y " +(x<y))
+console.log('x!=y '+ (x!=y))
+
+//Logical Operators
+
+const a =5;
+const b= 10;
+const c= 23;
+
+console.log(a>b && b>c);
+console.log(a>b||b>c);
+console.log(a!=b);
+
+
