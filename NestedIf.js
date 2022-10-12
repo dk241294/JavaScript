aff9dd2a3f757821f5735976bcec1f02eda7c356
dk241294 <deepak24121994@gmail.com>
2022-10-12 20:13:58 +0530

    Nested If satement

---
+++ NestedIf.js
@@ -0,0 +1,11 @@
+let x = -10;
+
+if (x > 0) {
+  console.log("Number is Positive");
+} else if (x == 0) {
+  console.log("Number is Zero");
+} else if (x < 0) {
+  console.log("Number is Negative");
+}
+
+
