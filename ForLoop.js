33c3080d84b478ed3b5fb91b02ef2c5b2a8a3352
dk241294 <deepak24121994@gmail.com>
2022-10-12 21:19:25 +0530

    For loop code

---
+++ ForLoop.js
@@ -0,0 +1,31 @@
+
+/* for (initialExpression; condition; updateExpression) {
+        // for loop body
+}
+*/ 
+// Print user name 5 times
+var User ="Deepak Kumar"
+var count=0
+for (var i=0;i<5;i++){
+console.log("My name is " +User)
+count++
+}
+console.log(count)
+
+
+//print sum of n natual number
+
+var n =100;
+var sum =0
+for (var i=1;i<=100;i++){
+    sum=sum+i
+    
+}
+console.log(sum)
+
+//or 
+var sum2=0
+for (var i=100;i>=1;i--){
+    sum2=sum2+i
+}
+console.log(sum)
