f51aa83359a54213f6eba053f0158da788000af5
dk241294 <deepak24121994@gmail.com>
2022-10-29 20:05:54 +0530

    Js

---
+++ FunctionsInJs.js
@@ -0,0 +1,23 @@
+
+//1.Function
+function number(){
+    console.log(5);
+}
+number();
+number();
+
+
+
+var a = parseInt  (prompt("Enter 1st Number"))
+var b =  parseInt (prompt("Enter 2nd Number") )
+
+//2.Function with Parameters
+ s=0
+function add (a,b){  
+     s= a+b;
+     console.log(s);
+}
+add(a,b);
+add(100,132);
+
+
